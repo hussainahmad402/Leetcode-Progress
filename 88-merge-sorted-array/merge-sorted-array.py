0067a486f64d37f31df32b91a272d71925d6bf69
Hussain Ahmad Madni <139582783+hussainahmad402@users.noreply.github.com>
2024-11-12 22:24:27 +0500

    Time: 0 ms (100.00%) | Memory: 16.6 MB (45.90%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,6 +3,6 @@ class Solution:
         j=len(nums1)-1
         for i in range(n):
             nums1[j]=nums2[i]
-            j-=1
+            j-=1 
         nums1[:]=sorted(nums1)
         return nums1
